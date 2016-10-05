e7d7bb020657c8a14b7f17fbd12bbf62fe36c086
Faaux <c.brammer1@gmail.com>
2016-10-05 13:21:40 +0200

    More build automation and added Casey Muratoris ctime to the project

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 
-REM ctime -begin cbLib.ctm
+ctime -begin cbLib.ctm
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
 set ClangCompilerFlags= -c -x c++ -mthread-model posix -fmath-errno -D _DEBUG -D _WINDOWS -D _UNICODE -D UNICODE -g2 -gdwarf-2 -O0 -Wall -Werror  -std=c++14 -fcxx-exceptions -Wno-unused-function -Wno-unused-variable
 set CommonCompilerFlags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -FC -Z7 /EHsc
 
@@ -38,6 +41,6 @@ popd
 xcopy "ThirdParty\bin" ".\bin" /S /Y /q
 xcopy ".\res" ".\bin" /S /Y /q
 
-REM ctime -end cbLib.ctm %LastError%
-
+ctime -end cbLib.ctm %LastError%
+pause
 exit
